c5f645e2247e95b460ae0431886b4695d7ec573a
Tomer Inbar <tomerinbar1@gmail.com>
2023-05-10 16:02:25 +0300

    Storage and rendering works - Gmail app

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,5 +1,28 @@
+const { useEffect, useState } = React
 
-export function MailIndex() {
-    return <div>mail app</div>
+import { MailService } from '../services/MailService.js'
+import { MailList } from '../cmps/MailList.jsx'
+
+export const MailIndex = () => {
+  const [mails, setMails] = useState([])
+
+  useEffect(() => {
+    loadMails()
+  }, [])
+
+  const loadMails = () => {
+    MailService.query().then(mail => {
+      setMails(mail)
+    })
+  }
+
+  return (
+    <section className="mail-app flex">
+      <div className="right-side">
+        // MailFilter
+        <MailList mails={mails} /> inside MailPreview
+      </div>
+      <div className="left-side flex column">// New Mail // MailMenu</div>
+    </section>
+  )
 }
-
