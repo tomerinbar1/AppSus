2d2c962e9d4c9e480783a7764e468cf3c683f4db
Tomer Inbar <tomerinbar1@gmail.com>
2023-05-11 15:25:24 +0300

    Removing Item functional and Mail Expand is WIP

--- apps/mail/cmps/MailExpand.jsx
+++ apps/mail/cmps/MailExpand.jsx
@@ -1,8 +1,21 @@
-const { Fragment, useState } = React
+const { useState } = React
 
 export const MailExpand = ({ mail }) => {
   const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
-    <li onClick={() => setIsExpanded(prevIsExpanded => !prevIsExpanded)}> </li>
+    <li onClick={toggleExpand}>
+      {isExpanded && (
+        <div>
+          <p>{mail.body}</p>
+          <p>Sent by: {mail.from.name} &lt;{mail.from.email}&gt;</p>
+          <p>Sent at: {new Date(mail.sentAt).toLocaleString()}</p>
+        </div>
+      )}
+    </li>
   )
 }
